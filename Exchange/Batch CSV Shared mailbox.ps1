bee7f2bf8ca5f97b211cb162ed921d839a218d2f
Brian Stark <BStarkIT@Gmail.com>
2024-12-17 09:07:55 +0000

    update

--- Exchange/Batch CSV Shared mailbox.ps1
+++ Exchange/Batch CSV Shared mailbox.ps1
@@ -1,2 +1,2 @@
 
-Import-CSV C:\PS\EdinburghUsers.csv | foreach { Add-MailboxPermission  -Identity TestMB@scotcourts.gov.uk -User  $_.SamAccountName  -AccessRights  FullAccess }
+Import-CSV C:\PS\EdinburghUsers.csv | ForEach-Object { Add-MailboxPermission  -Identity TestMB@scotcourts.gov.uk -User  $_.SamAccountName  -AccessRights  FullAccess }
