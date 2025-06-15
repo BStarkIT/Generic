ce4d552ee1c917783fc2c99925e0e705b1e898f7
Brian Stark <BStarkit@gmail.com>
2025-06-15 18:43:24 +0100

    15-06-25

--- Tools/# Find_Duplicates.ps1
+++ Tools/# Find_Duplicates.ps1
@@ -18,7 +18,7 @@ $Input_1 = $args[0]
 $Input_2 = $args[1]
 
 #Create Log
-$Output = "C:\PS\Log.csv"
+$Output = "D:\Log.csv"
 Add-Content $Output 'Original,Status,Duplicate'
 
 #Directory Check
@@ -30,12 +30,14 @@ If ($ExtChk_1 -eq '' -And $ExtChk_2 -eq '') {
     #Grab list of files from each input path
     $Files_1 = Get-ChildItem -path $Input_1 -include $Off_Array -recurse -file
     $Files_2 = Get-ChildItem -path $Input_2 -include $Off_Array -recurse -file
+
     #Grab next file from each input path
     ForEach ($File_1 in $Files_1) {
         $FileCheck = 0
         $Hash_1 = Get-FileHash $File_1.FullName
         ForEach ($File_2 in $Files_2) {
             $Hash_2 = Get-FileHash $File_2.FullName
+
             #Compare file hashes
             If ($Hash_1.Hash -eq $Hash_2.Hash) {
                 #Output results to csv
